a280646b6024a7a943ea13d471128d9b79cfa933
kamelzcs <kamelzcs@gmail.com>
2014-02-14 12:30:05 +0900

    can access the view statistics

---
+++ rstudio.R
@@ -0,0 +1,18 @@
+library(devtools)
+library(rga)
+rga.open(instance="ga", where = "~/Documents/R/ga-api")
+ga$status()
+# Use a call to the Management API to query your Profiles and select among them
+profiles<-ga$getProfiles()
+profiles
+# Define the ids and web properties to work with. The web.proporties is reserved for
+# the name of the ids for labeling reasons etc
+ids<-profiles[1]
+start.date = "2014-01-01"
+end.date = today()-1
+speed = ga$getData(30593836, start.date, end.date, 
+                   metrics = "ga:visits,ga:pageviews", dimensions = "ga:date", 
+                   sort = "", filters = "", segment = "",
+                   start = 1, max = 1000)
+
+speed
