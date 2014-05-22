9a0b82ceed466d992192223f5e611bcf6ff11404
kamelzcs <kamelzcs@gmail.com>
2014-05-22 18:27:36 +0900

    recruit path

--- recruit_edit.R
+++ recruit_edit.R
@@ -2,7 +2,7 @@ library(tools)
 library(devtools)
 library(rga)
 
-key_words = "^/diarypost/diary"
+key_words = "diarypost/diary"
 smart_pc_list = list(30593836, 26708871)
 list_name = list("smart", "pc");
 
@@ -26,13 +26,13 @@ profiles
 # the name of the ids for labeling reasons etc
 ids<-profiles[1]
 start.date = "2014-05-01"
-end.date = "2014-05-22"
-filter_words = paste("ga:pagePath=~", key_words, sep="")
+end.date = "2014-05-20"
+filter_words = paste("ga:nextPagePath=~", key_words, sep="")
 filter_words
 LL = list()
 for (i in 1:2){
     LL[[i]] = ga$getData(smart_pc_list[[i]], start.date, end.date,
-                       metrics = "ga:visits,ga:pageViews,ga:exits,ga:pageviewsPerVisit,ga:entranceRate,ga:avgTimeOnpage,ga:exitRate", dimensions = "ga:pagePath",
+                       metrics = "ga:visits,ga:pageViews,ga:exits,ga:pageviewsPerVisit,ga:entranceRate,ga:avgTimeOnpage,ga:exitRate",dimensions = "ga:previousPagePath,ga:nextPagePath",
                        sort = "-ga:pageViews", filters = filter_words, segment = "",
                        start = 1, max = 150)
 }
@@ -52,4 +52,5 @@ nowPath = paste(timeStamp, nowPath, sep="-")
 nowPath
 total_path = paste(dir_name, nowPath, sep= "/")
 total_path
-write.csv(merged.df, total_path)
+write.csv(LL[[1]], total_path)
+write.table(LL[[2]], file = total_path, append=TRUE, sep=",", col.names=TRUE)
